49a2bce1f65f6969386988f649b6b32f3f15f3ca
attilafulop <ati.fulop.82@gmail.com>
2023-11-04 21:41:42 +0000

    Hero, PopularP, SuperQ added

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+
+export {
+	Hero,
+	PopularProducts,
+	SuperQuality,
+	SpecialOffers,
+	Footer,
+	CustomerReviews,
+	Services,
+	Subscribe,
+};
